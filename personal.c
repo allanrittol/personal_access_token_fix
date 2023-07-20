e5349c8cc33d153d9f657505b81fa85593c4139a
Allan Kamau <allanrittol@outlook.com>
2023-07-20 00:04:40 -0700

    personal

---
+++ personal.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - this is the entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		printf("%d ", i);
+	}
+	return (0);
+}
